21aef2bd61dde3be520abda83da28888ea53a5bc
kimtaes <kimtaes@oregonstate.edu>
2021-01-31 13:47:28 +0900

    Update leap_year.py
    Second commit

--- leap_year.py
+++ leap_year.py
@@ -1,10 +1,14 @@
 while True:
+    is_leap_year = None
 
     year = int(input())
 
     if year % 4 == 0:
         if year % 100 == 0:
-            is_leap_year = False
+            if year % 400 == 0:
+                is_leap_year = True
+            else:
+                is_leap_year = False
         else:
             is_leap_year = True
     else:
