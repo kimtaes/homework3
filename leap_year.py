7f47920116a641d781a8cbd2f8cd6a9bc79b6cb9
kimtaes <kimtaes@oregonstate.edu>
2021-01-31 13:46:03 +0900

    Create leap_year.py
    First commit

---
+++ leap_year.py
@@ -0,0 +1,16 @@
+while True:
+
+    year = int(input())
+
+    if year % 4 == 0:
+        if year % 100 == 0:
+            is_leap_year = False
+        else:
+            is_leap_year = True
+    else:
+        is_leap_year = False
+
+    if is_leap_year:
+        print(f'{year} is a leap year')
+    else:
+        print(f'{year} is not a leap year')
